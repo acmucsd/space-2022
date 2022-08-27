6fadf78204dacda60067d88ca7b71b21224487f3
ryantruong25 <75468904+ryan-truong@users.noreply.github.com>
2022-08-27 10:16:08 -0700

    hero page  Please enter the commit message for your changes. Lines starting

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -5,76 +5,10 @@ import spaceship from './images/space_spaceship.png';
 import trail from './images/space_trail.png';
 
 import Image from 'next/image';
-import Particles from "react-particles";
-import { loadFull } from "tsparticles";
-import { useCallback } from 'react'
 
 const Hero: React.FC = () => {
 
-  const particlesInit = useCallback(async (engine: Engine) => {
-    console.log(engine);
-    await loadFull(engine);
-  }, []);
-
-  const particlesLoaded = useCallback(async (container: Container | undefined) => {
-      await console.log(container)
-  }, []);
-
   return <>
-   <Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={{
-        fpsLimit: 120,
-        particles: {
-          color: {
-            value: "#FFFFFF",
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outModes: {
-              default: "bounce",
-            },
-            random: false,
-            speed: 0,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800,
-            },
-            value: 90,
-          },
-          opacity: {
-            animation: {
-              enable: true,
-              speed: 1,
-              sync: false,
-              opacity_min: 0
-            },
-            value: 1
-          },
-          wobble:{
-            enable: true,
-            distance: 0.25,
-            speed: 0.5
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            value: { min: 1, max: 4 },
-          },
-        },
-        detectRetina: true,
-      }}
-    />
     <div className = {s.heroContainer}>
       <div className = {s.title}>
         <Image src={title} alt = "space"></Image>
