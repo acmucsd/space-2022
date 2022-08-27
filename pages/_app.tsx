5941152758a66715d07545b74750750eb809857c
Justin Sin <48116685+Jvil26@users.noreply.github.com>
2022-08-26 21:56:30 -0700

    about page (#1)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
-import 'src/styles/globals.scss';
-import 'src/styles/reset.scss';
+import "src/styles/globals.scss";
+import "src/styles/reset.scss";
 
-import type { AppProps } from 'next/app';
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
-import type { NextPage } from 'next'
-import About from 'src/sections/About';
-import Companies from 'src/sections/Companies';
-import FAQ from 'src/sections/FAQ';
-import Hero from 'src/sections/Hero';
-import style from 'src/styles/Home.module.scss';
+import type { NextPage } from "next";
+import Companies from "src/sections/Companies";
+import About from "src/sections/About";
+import FAQ from "src/sections/FAQ";
+import Hero from "src/sections/Hero";
+import StarCanvas from "src/components/StarCanvas";
+
+import "src/styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
-    <div className={style.container}>
+    <main>
+      <StarCanvas />
       <Hero />
-      <About />
       <FAQ />
+      <About />
       <Companies />
-    </div>
-  )
-}
+    </main>
+  );
+};
 
-export default Home
+export default Home;
