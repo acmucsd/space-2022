572b1c12ff71fc608b4f2854032c22ad847d8d71
ryantruong25 <75468904+ryan-truong@users.noreply.github.com>
2022-09-06 16:39:32 -0700

    Fixed image locations

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -1,8 +1,8 @@
 import s from "./style.module.scss";
-import title from './images/space_title.png';
-import planet from './images/space_planet.png';
-import spaceship from './images/space_spaceship.png';
-import trail from './images/space_trail.png';
+// import title from './images/space_title.png';
+// import planet from './images/space_planet.png';
+// import spaceship from './images/space_spaceship.png';
+// import trail from './images/space_trail.png';
 
 import Image from 'next/image';
 
@@ -11,13 +11,13 @@ const Hero: React.FC = () => {
   return <>
     <div className = {s.heroContainer}>
       <div className = {s.title}>
-        <Image src={title} alt = "space"></Image>
+        <img src="asset/space_title.png" alt = "space"/>
       </div>
       <div className = {s.spaceship}>
-        <Image src={spaceship} alt="spaceship"></Image>
+        <img src="asset/space_spaceship.png" alt="spaceship"/>
       </div>
       <div className = {s.trail}>
-        <Image src={trail} alt="trail"></Image>
+        <img src="asset/space_trail.png" alt="trail"/>
       </div>
       <div className = {s.heroDescription}>
         <div className = {s.heroSubtitle}>StartuP: ACM&apos;s Career Expo</div>
@@ -28,7 +28,7 @@ const Hero: React.FC = () => {
         </div>
       </div>
       <div className = {s.planet}>
-        <Image src={planet} alt="planet"></Image>
+        <img src="asset/space_planet.png" alt="planet"/>
       </div>
     </div>
   
