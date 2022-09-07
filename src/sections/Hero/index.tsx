4d3082c1f0472499682115abec4545c2bc9f372b
ryantruong25 <75468904+ryan-truong@users.noreply.github.com>
2022-09-06 21:37:17 -0700

    Fixed positioning issues

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -4,7 +4,7 @@ const Hero: React.FC = () => {
 
   return <>
     <div className = {s.heroContainer}>
-      <img src="asset/hero_title.png" alt="hero" className = {s.heroTitle}/>
+      <img src="asset/hero_image.png" alt="hero" className = {s.heroTitle}/>
       <div className = {s.heroDescription}>
         <div className = {s.heroSubtitle}>StartuP: ACM&apos;s Career Expo</div>
         <div className = {s.heroInfo}>
@@ -12,7 +12,6 @@ const Hero: React.FC = () => {
           <div>November 15th, 2022 @ TBD</div>
           <button className = {s.registerButton}>Register Today!</button>
         </div>
-        <img src="asset/space_planet.png" alt="planet" className = {s.heroPlanet}/>
       </div>
     </div>
   </>
