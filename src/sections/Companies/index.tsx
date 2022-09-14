c5ff477674c59ca2986ef73db2dbe50492fa406b
Justin Sin <48116685+Jvil26@users.noreply.github.com>
2022-09-14 14:35:53 -0700

    Companies Page

--- src/sections/Companies/index.tsx
+++ src/sections/Companies/index.tsx
@@ -1,5 +1,15 @@
+import CompaniesGrid from "src/components/CompaniesGrid";
+import s from "./style.module.scss";
+
 const Companies: React.FC = () => {
-  return <div></div>
-}
+  return (
+    <section className={s.container}>
+      <div className={s.content}>
+        <h1>Participating Companies</h1>
+        <CompaniesGrid />
+      </div>
+    </section>
+  );
+};
 
 export default Companies;
