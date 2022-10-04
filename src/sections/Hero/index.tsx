3610833ce4d8615f2aa3903c7951236ec146776f
Ryan Truong <75468904+ryan-truong@users.noreply.github.com>
2022-10-03 23:06:30 -0700

    Hero Page (#3)

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -1,5 +1,21 @@
+import s from "./style.module.scss";
+
 const Hero: React.FC = () => {
-  return <div></div>
+
+  return <>
+    <div className = {s.heroContainer}>
+      <img src="asset/hero_title.png" alt="hero" className = {s.heroTitle}/>
+      <img src="asset/hero_planet.png" alt="planet" className = {s.heroPlanet}/>
+      <div className = {s.heroDescription}>
+        <div className = {s.heroSubtitle}>StartuP: ACM&apos;s Career Expo</div>
+        <div className = {s.heroInfo}>
+          <div>Price Center West Ballroom</div>
+          <div>November 15th, 2022 @ TBD</div>
+          <button className = {s.registerButton}>Register Today!</button>
+        </div>
+      </div>
+    </div>
+  </>
 }
 
 export default Hero;
