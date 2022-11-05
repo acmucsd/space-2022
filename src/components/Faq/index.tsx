5d37e111796a482a2da9d6acc1ab71f73d25c324
ryantruong25 <75468904+ryan-truong@users.noreply.github.com>
2022-11-05 15:46:01 -0700

    Updated faq page and made fixes

--- src/components/Faq/index.tsx
+++ src/components/Faq/index.tsx
@@ -1,34 +1,41 @@
+import { useState } from 'react'
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
 import s from "./style.module.scss";
 
 interface panelData {
   data: {question: string; answer: string|any}[];
 }
 const Faq: React.FC<panelData> = ({ data }) => {
+  const [expandedIndex, setExpandedIndex] = useState<Number>(-1);
+
+  const handleChange =
+    (panelIndex: Number) => (event: React.SyntheticEvent, newExpanded: boolean) => {
+      setExpandedIndex(newExpanded ? panelIndex : -1);
+    };
+
   return (
     <>
         {data.map((questionObject, index) => 
-          <Accordion sx = {{ 
-            borderTop: index === 0 ? 3 : 0,
-            borderBottom: 3,
-            bgcolor: 'transparent',
-            color: 'white',
-            boxShadow: 'none',
-          }} key = {questionObject.question}>
-            <AccordionSummary expandIcon={<ExpandMoreIcon className = {s.header}/>}>
-              <Typography className = {s.header}>{questionObject.question}</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography component="div" className = {s.description}>
-                <div>{questionObject.answer}</div>
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
+            <Accordion expanded= {expandedIndex === index} onChange= {handleChange(index)} sx = {{ 
+              borderTop: index === 0 ? 3 : 0,
+              borderBottom: 3,
+              bgcolor: 'transparent',
+              color: 'white',
+              boxShadow: 'none',
+            }} key = {questionObject.question}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon className = {s.header}/>}>
+                <Typography className = {s.header}>{questionObject.question}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography component="div" className = {s.description}>
+                  <div>{questionObject.answer}</div>
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
         )}
   </>
   )
