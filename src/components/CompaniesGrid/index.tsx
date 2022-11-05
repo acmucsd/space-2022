6a59c2e30f4085eb60f99f84b9b01e8144700d6f
Justin Sin <48116685+Jvil26@users.noreply.github.com>
2022-11-05 16:10:38 -0700

    Improved styling for company image

--- src/components/CompaniesGrid/index.tsx
+++ src/components/CompaniesGrid/index.tsx
@@ -1,21 +1,26 @@
 import s from "./style.module.scss";
 
 const companies = [
-  "Merck",
-  "MaXentric",
-  "Company Name",
-  "Company Name",
-  "Company Name",
-  "Company Name",
+  {
+    name: "Merck",
+    img: "asset/about-star.png",
+    link: "https://www.google.com/",
+  },
+  { name: "Merck", img: "asset/about-star.png", link: "" },
+  { name: "Merck", img: "asset/about-star.png", link: "" },
+  { name: "Merck", img: "asset/about-star.png", link: "" },
+  { name: "Merck", img: "asset/about-star.png", link: "" },
+  { name: "Merck", img: "asset/about-star.png", link: "" },
 ];
 
 const CompaniesGrid: React.FC = () => {
   return (
     <div className={s.container}>
-      {companies.map((companyName, index) => (
-        <div className={s.companyItem} key={index}>
-          <h5>{companyName}</h5>
-        </div>
+      {companies.map((company, index) => (
+        <a className={s.companyItem} key={index} href={company.link}>
+          <img src={company.img} className={s.img} alt="About Star Image" />
+          <h5>{company.name}</h5>
+        </a>
       ))}
     </div>
   );
